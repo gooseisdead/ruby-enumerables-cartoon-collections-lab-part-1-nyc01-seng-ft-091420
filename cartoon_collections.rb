25826874f6906f74bbd34282d262bc349d4df97f
Mike Smith <gooseisdead@mac.com>
2020-09-04 17:34:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |dwarf|
-    puts "#{dwarf}"
+  array.each_with_index do |dwarf, index|
+    puts "#{index}. #{dwarf}"
   end
   end
   # Use `each_with_index` to enumerate over the provided array
